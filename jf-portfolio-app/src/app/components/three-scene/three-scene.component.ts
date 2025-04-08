2da864d2e0fef8ecbb087d0280b6964d65f376d2
JannatulFardouse <jannatul.fardouse.cse@gmail.com>
2025-04-08 10:52:39 +0200

    Install and Create all component for the portfolio

---
+++ jf-portfolio-app/src/app/components/three-scene/three-scene.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-three-scene',
+  standalone: true,
+  imports: [],
+  templateUrl: './three-scene.component.html',
+  styleUrl: './three-scene.component.scss'
+})
+export class ThreeSceneComponent {
+
+}
