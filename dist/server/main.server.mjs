5995f4fc685b30929388dd2cb3c92eda3a31a1ed
JannatulFardouse <jannatul.fardouse.cse@gmail.com>
2025-04-16 17:46:07 +0200

    try to publish gh-pages

--- dist/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-RIPC7B32.mjs";import"./chunk-OZOEAINE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- jf-portfolio-app/src/main.ts
+++ jf-portfolio-app/src/main.ts
@@ -2,8 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations'; 
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideAnimationsAsync()],
+  providers: [
+    provideRouter(routes),
+    provideAnimations() 
+  ],
 });
